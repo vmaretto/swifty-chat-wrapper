4acfb7ab4d1d6cef1a27410ec9529f9195e8c15b
vmaretto <info@posti.world>
2025-10-07 00:37:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // IMPORTANTE: Sostituisci 'your-repo-name' con il nome del tuo repository GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/swifty-chat-wrapper/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
