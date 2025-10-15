48c2d80ae9ae17167e0eb181b9161495ebd2d216
vmaretto <info@posti.world>
2025-10-15 00:57:34 +0000

    fix: add content-type header to ping function

--- api/ping.js
+++ api/ping.js
@@ -1,2 +1,9 @@
-export const config={runtime:'edge'};
-export default async()=>new Response('ok',{status:200});
+export const config = { runtime: 'edge' };
+
+export default async () =>
+  new Response("pong 🟢 Edge Function attiva!", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+    },
+  });
